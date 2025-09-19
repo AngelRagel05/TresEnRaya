cbc5b940f4e7bb5795c8c151c26311fa69083cba
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-09-19 14:21:58 +0200

    Refactor métodos de verificación de victoria en la clase Tablero para simplificar la lógica y mejorar la legibilidad. Corregir la representación de fichas en el método toString.

--- src/main/java/model/Tablero.java
+++ src/main/java/model/Tablero.java
@@ -40,10 +40,10 @@ public class Tablero {
     private Boolean ganaHorizontal(Ficha ficha) {
         for (int i = 0; i < casillas.length; i++) {
             int cont = 0;
-            int j = 0;
-            cont = (casillas[i][j] != ficha) ? cont = 0 : cont++;
-
-            if (cont == 3) return true;
+            for (int j = 0; j < casillas.length; j++) {
+                cont = (casillas[i][j] == ficha) ? cont + 1 : 0;
+                if (cont == 3) return true;
+            }
         }
         return false;
     }
@@ -51,52 +51,28 @@ public class Tablero {
     private Boolean ganaVertical(Ficha ficha) {
         for (int j = 0; j < casillas.length; j++) {
             int cont = 0;
-            int i = 0;
-            if (casillas[i][j] == ficha) {
-                cont++;
-            } else {
-                cont = 0;
-            }
-
-            if (cont == 3) {
-                return true;
+            for (int i = 0; i < casillas.length; i++) {
+                cont = (casillas[i][j] == ficha) ? cont + 1 : 0;
+                if (cont == 3) return true;
             }
         }
         return false;
     }
 
     private Boolean ganaDiagonalAnversa(Ficha ficha) {
+        int cont = 0;
         for (int i = 0; i < casillas.length; i++) {
-            int cont = 0;
-            int j = 0;
-            if (casillas[i][j] == ficha) {
-                cont++;
-                j++;
-            } else {
-                cont = 0;
-            }
-
-            if (cont == 3) {
-                return true;
-            }
+            cont = (casillas[i][i] == ficha) ? cont + 1 : 0;
+            if (cont == 3) return true;
         }
         return false;
     }
 
     private Boolean ganaDiagonalInvertida(Ficha ficha) {
-        for (int j = casillas.length; j > 0; j--) {
-            int cont = 0;
-            int i = 0;
-            if (casillas[i][j] == ficha) {
-                cont++;
-                i++;
-            } else {
-                cont = 0;
-            }
-
-            if (cont == 3) {
-                return true;
-            }
+        int cont = 0;
+        for (int i = casillas.length; i > 0; i--) {
+            cont = (casillas[i][i] == ficha) ? cont + 1 : 0;
+            if (cont == 3) return true;
         }
         return false;
     }
@@ -108,12 +84,17 @@ public class Tablero {
             for (int j = 0; j < casillas.length; j++) {
                 sb.append(
                         (casillas[i][j] == Ficha.O) ? "O" :
-                        (casillas[i][j] == Ficha.X) ? "0" : "-"
+                        (casillas[i][j] == Ficha.X) ? "X" : "-"
                 );
                 if (j < casillas[i].length - 1) sb.append(" ");
             }
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    private Object valueOf(Ficha ficha) {
+        return (ficha == Ficha.O) ? "O" :
+                (ficha == Ficha.X) ? "X" : "-";
     }
 }
