f9e3d12b4b10f70c5ccffc931b6202e08435565d
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-09-20 09:16:55 +0200

    Crear la clase Juego para iniciar una nueva partida y mostrar un mensaje de inicio.

--- src/main/java/Iniciar.java
+++ src/main/java/Iniciar.java
@@ -2,5 +2,7 @@ import model.Partida;
 public class Juego {
     public static void main(String[] args) {
         Partida juego = new Partida();
+        System.out.println("Inicio del juego.");
+        
     }
 }
